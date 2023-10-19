0c7896ffeb357548315e15e365f49a610f0ee2a9
reginawiti <143196467+reginawiti@users.noreply.github.com>
2023-10-19 00:42:03 -0700

    Add files via upload

---
+++ python test one/variables.py
@@ -0,0 +1,19 @@
+Student Marks 
+total_Average Marks 
+# total_Age =(22)
+total= sum ={22/2=11}
+
+
+        qn 2 def average (n1,n2,n3
+        totral= int(n1) +int (n2) +(n3)
+        Avg=total 1/3
+        return Avg
+        
+        print (average (1,2,3)# i have passed 3 arguments 
+        output -----
+        6
+        
+        
+        3 student _marks =[78,83,90,88,75}=414
+        #total_sum=[414+96}=510]
+        ]))
